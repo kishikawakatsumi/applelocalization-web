69530d16c378e154694f4e928306ceba73f109a1
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2023-09-24 04:08:03 +0900

    Update languages

--- models/macos12/language_mappings.ts
+++ models/macos12/language_mappings.ts
@@ -1,90 +1,219 @@
-Base
-Dutch
-English
-French
-German
-Italian
-Japanese
-Spanish
-ar
-az
-bg
-bn
-bn_Latn
-ca
-cs
-da
-de
-el
-en
-en-GB
-en-US
-en_AU
-en_CA
-en_GB
-en_IN
-es
-es_419
-es_MX
-et
-fi
-fil
-fr
-fr-CA
-fr_CA
-gu_Latn
-he
-hi
-hi_Latn
-hr
-hu
-hy
-id
-is
-it
-ja
-ka
-kk
-km
-kn_Latn
-ko
-lo
-lt
-lv
-ml_Latn
-mn
-mr
-mr_Latn
-ms
-my
-nb
-nl
-no
-or_Latn
-pa
-pa_Latn
-pl
-pt
-pt-PT
-pt_BR
-pt_PT
-ro
-ru
-sk
-sq
-sv
-ta
-ta_Latn
-te_Latn
-th
-tr
-uk
-ur
-uz
-vi
-yue_CN
-zh-Hans
-zh-Hant
-zh_CN
-zh_HK
-zh_TW
+export const languageMapping: { [key: string]: string[] } = {
+  English: [
+    "Base",
+    "English",
+    "en",
+    "en-GB",
+    "en-US",
+    "en_AU",
+    "en_CA",
+    "en_GB",
+    "en_IN",
+  ],
+  French: [
+    "French",
+    "fr",
+    "fr-CA",
+    "fr_CA",
+  ],
+  German: [
+    "German",
+    "de",
+  ],
+  Italian: [
+    "Italian",
+    "it",
+  ],
+  Japanese: [
+    "Japanese",
+    "ja",
+  ],
+  Spanish: [
+    "Spanish",
+    "es",
+    "es_419",
+    "es_MX",
+  ],
+  Albanian: [
+    "sq",
+  ],
+  Arabic: [
+    "ar",
+  ],
+  Armenian: [
+    "hy",
+  ],
+  Azerbaijani: [
+    "az",
+  ],
+  Bengali: [
+    "bn",
+    "bn_Latn",
+  ],
+  Bulgarian: [
+    "bg",
+  ],
+  Burmese: [
+    "my",
+  ],
+  Catalan: [
+    "ca",
+  ],
+  "Central Khmer": [
+    "km",
+  ],
+  Croatian: [
+    "hr",
+  ],
+  Czech: [
+    "cs",
+  ],
+  Danish: [
+    "da",
+  ],
+  Dutch: [
+    "Dutch",
+    "nl",
+  ],
+  Georgian: [
+    "ka",
+  ],
+  Greek: [
+    "el",
+  ],
+  Estonian: [
+    "et",
+  ],
+  Filipino: [
+    "fil",
+  ],
+  Finnish: [
+    "fi",
+  ],
+  Gujarati: [
+    "gu_Latn",
+  ],
+  Hebrew: [
+    "he",
+  ],
+  Hindi: [
+    "hi",
+    "hi_Latn",
+  ],
+  Hungarian: [
+    "hu",
+  ],
+  Icelandic: [
+    "is",
+  ],
+  Indonesian: [
+    "id",
+  ],
+  Kannada: [
+    "kn_Latn",
+  ],
+  Kazakh: [
+    "kk",
+    "kk-iphoneos",
+    "kk-macos",
+    "kk-tvos",
+    "kk~applewatch",
+    "kk~iphone",
+  ],
+  Korean: [
+    "ko",
+  ],
+  Lao: [
+    "lo",
+  ],
+  Latvian: [
+    "lv",
+  ],
+  Lithuanian: [
+    "lt",
+  ],
+  Malayalam: [
+    "ml_Latn",
+  ],
+  Marathi: [
+    "mr",
+    "mr_Latn",
+  ],
+  Malay: [
+    "ms",
+  ],
+  Mongolian: [
+    "mn",
+  ],
+  Norwegian: [
+    "no",
+  ],
+  "Norwegian Bokmål": [
+    "nb",
+  ],
+  Oriya: [
+    "or_Latn",
+  ],
+  Punjabi: [
+    "pa",
+    "pa_Latn",
+  ],
+  Polish: [
+    "pl",
+  ],
+  Portuguese: [
+    "pt",
+    "pt-PT",
+    "pt_BR",
+    "pt_PT",
+  ],
+  Romanian: [
+    "ro",
+  ],
+  Russian: [
+    "ru",
+  ],
+  Slovak: [
+    "sk",
+  ],
+  Swedish: [
+    "sv",
+  ],
+  Tamil: [
+    "ta",
+    "ta_Latn",
+  ],
+  Telugu: [
+    "te_Latn",
+  ],
+  Thai: [
+    "th",
+  ],
+  Turkish: [
+    "tr",
+  ],
+  Ukrainian: [
+    "uk",
+  ],
+  Urdu: [
+    "ur",
+  ],
+  Uzbek: [
+    "uz",
+  ],
+  Vietnamese: [
+    "vi",
+  ],
+  "Simplified Chinese": [
+    "zh-Hans",
+    "zh_CN",
+  ],
+  "Traditional Chinese": [
+    "zh-Hant",
+    "zh_HK",
+    "zh_TW",
+  ],
+  Cantonese: [
+    "yue-CN",
+  ],
+};
--- models/macos13/language_mappings.ts
+++ models/macos13/language_mappings.ts
@@ -1,316 +1,454 @@
-Base
-Dutch
-English
-French
-German
-Italian
-Japanese
-Spanish
-ar
-ar-iphoneos
-ar-macos
-ar-tvos
-ar~applewatch
-ar~ipad
-ar~iphone
-az
-bn
-ca
-ca-iphoneos
-ca-macos
-ca-tvos
-ca~applewatch
-ca~ipad
-ca~iphone
-cs
-cs-iphoneos
-cs-macos
-cs-tvos
-cs~applewatch
-cs~ipad
-cs~iphone
-da
-da-iphoneos
-da-macos
-da-tvos
-da~applewatch
-da~ipad
-da~iphone
-de
-de-iphoneos
-de-macos
-de-tvos
-de~applewatch
-de~ipad
-de~iphone
-el
-el-iphoneos
-el-macos
-el-tvos
-el~applewatch
-el~ipad
-el~iphone
-en
-en-GB
-en-iphoneos
-en-macos
-en-tvos
-en_AU
-en_AU-iphoneos
-en_AU-macos
-en_AU-tvos
-en_AU~applewatch
-en_AU~ipad
-en_AU~iphone
-en_CA
-en_GB
-en_GB-iphoneos
-en_GB-macos
-en_GB-tvos
-en_GB~applewatch
-en_GB~ipad
-en_GB~iphone
-en_IN
-en~applewatch
-en~ipad
-en~iphone
-es
-es-iphoneos
-es-macos
-es-tvos
-es_419
-es_419-iphoneos
-es_419-macos
-es_419-tvos
-es_419~applewatch
-es_419~ipad
-es_419~iphone
-es_MX
-es~applewatch
-es~ipad
-es~iphone
-et
-fi
-fi-iphoneos
-fi-macos
-fi-tvos
-fil
-fi~applewatch
-fi~ipad
-fi~iphone
-fr
-fr-CA
-fr-iphoneos
-fr-macos
-fr-tvos
-fr_CA
-fr_CA-iphoneos
-fr_CA-macos
-fr_CA-tvos
-fr_CA~applewatch
-fr_CA~ipad
-fr_CA~iphone
-fr~applewatch
-fr~ipad
-fr~iphone
-he
-he-iphoneos
-he-macos
-he-tvos
-he~applewatch
-he~ipad
-he~iphone
-hi
-hi-iphoneos
-hi-macos
-hi-tvos
-hi_Latn
-hi~applewatch
-hi~ipad
-hi~iphone
-hr
-hr-iphoneos
-hr-macos
-hr-tvos
-hr~applewatch
-hr~ipad
-hr~iphone
-hu
-hu-iphoneos
-hu-macos
-hu-tvos
-hu~applewatch
-hu~ipad
-hu~iphone
-hy
-id
-id-iphoneos
-id-macos
-id-tvos
-id~applewatch
-id~ipad
-id~iphone
-is
-it
-it-iphoneos
-it-macos
-it-tvos
-it~applewatch
-it~ipad
-it~iphone
-ja
-ja-iphoneos
-ja-macos
-ja-tvos
-ja~applewatch
-ja~ipad
-ja~iphone
-ka
-km
-ko
-ko-iphoneos
-ko-macos
-ko-tvos
-ko~applewatch
-ko~ipad
-ko~iphone
-lo
-lt
-lv
-mn
-mr
-ms
-ms-iphoneos
-ms-macos
-ms-tvos
-ms~applewatch
-ms~ipad
-ms~iphone
-my
-nb
-nl
-nl-iphoneos
-nl-macos
-nl-tvos
-nl~applewatch
-nl~ipad
-nl~iphone
-no
-no-iphoneos
-no-macos
-no-tvos
-no~applewatch
-no~ipad
-no~iphone
-pa
-pl
-pl-iphoneos
-pl-macos
-pl-tvos
-pl~applewatch
-pl~ipad
-pl~iphone
-pt
-pt-PT
-pt-iphoneos
-pt-macos
-pt_BR
-pt_BR-iphoneos
-pt_BR-macos
-pt_BR-tvos
-pt_BR~ipad
-pt_BR~iphone
-pt_PT
-pt_PT-iphoneos
-pt_PT-macos
-pt_PT-tvos
-pt_PT~applewatch
-pt_PT~ipad
-pt_PT~iphone
-pt~applewatch
-pt~ipad
-pt~iphone
-ro
-ro-iphoneos
-ro-macos
-ro-tvos
-ro~applewatch
-ro~ipad
-ro~iphone
-ru
-ru-iphoneos
-ru-macos
-ru-tvos
-ru~applewatch
-ru~ipad
-ru~iphone
-sk
-sk-iphoneos
-sk-macos
-sk-tvos
-sk~applewatch
-sk~ipad
-sk~iphone
-sq
-sv
-sv-iphoneos
-sv-macos
-sv-tvos
-sv~applewatch
-sv~ipad
-sv~iphone
-ta
-th
-th-iphoneos
-th-macos
-th-tvos
-th~applewatch
-th~ipad
-th~iphone
-tr
-tr-iphoneos
-tr-macos
-tr-tvos
-tr~applewatch
-tr~ipad
-tr~iphone
-uk
-uk-iphoneos
-uk-macos
-uk-tvos
-uk~applewatch
-uk~ipad
-uk~iphone
-ur
-uz
-vi
-vi-iphoneos
-vi-macos
-vi-tvos
-vi~applewatch
-vi~ipad
-vi~iphone
-yue_CN
-zh-Hans
-zh-Hant
-zh_CN
-zh_CN-iphoneos
-zh_CN-macos
-zh_CN-tvos
-zh_CN~applewatch
-zh_CN~ipad
-zh_CN~iphone
-zh_HK
-zh_HK-iphoneos
-zh_HK-macos
-zh_HK-tvos
-zh_HK~applewatch
-zh_HK~ipad
-zh_HK~iphone
-zh_TW
-zh_TW-iphoneos
-zh_TW-macos
-zh_TW-tvos
-zh_TW~applewatch
-zh_TW~ipad
-zh_TW~iphone
+export const languageMapping: { [key: string]: string[] } = {
+  English: [
+    "Base",
+    "English",
+    "en",
+    "en-GB",
+    "en-iphoneos",
+    "en-macos",
+    "en-tvos",
+    "en_AU",
+    "en_AU-iphoneos",
+    "en_AU-macos",
+    "en_AU-tvos",
+    "en_AU~applewatch",
+    "en_AU~ipad",
+    "en_AU~iphone",
+    "en_CA",
+    "en_GB",
+    "en_GB-iphoneos",
+    "en_GB-macos",
+    "en_GB-tvos",
+    "en_GB~applewatch",
+    "en_GB~ipad",
+    "en_GB~iphone",
+    "en_IN",
+    "en~applewatch",
+    "en~ipad",
+    "en~iphone",
+  ],
+  French: [
+    "French",
+    "fr",
+    "fr-CA",
+    "fr-iphoneos",
+    "fr-macos",
+    "fr-tvos",
+    "fr_CA",
+    "fr_CA-iphoneos",
+    "fr_CA-macos",
+    "fr_CA-tvos",
+    "fr_CA~applewatch",
+    "fr_CA~ipad",
+    "fr_CA~iphone",
+    "fr~applewatch",
+    "fr~ipad",
+    "fr~iphone",
+  ],
+  German: [
+    "German",
+    "de",
+    "de-iphoneos",
+    "de-macos",
+    "de-tvos",
+    "de~applewatch",
+    "de~ipad",
+    "de~iphone",
+  ],
+  Italian: [
+    "Italian",
+    "it",
+    "it-iphoneos",
+    "it-macos",
+    "it-tvos",
+    "it~applewatch",
+    "it~ipad",
+    "it~iphone",
+  ],
+  Japanese: [
+    "Japanese",
+    "ja",
+    "ja-iphoneos",
+    "ja-macos",
+    "ja-tvos",
+    "ja~applewatch",
+    "ja~ipad",
+    "ja~iphone",
+  ],
+  Spanish: [
+    "Spanish",
+    "es",
+    "es-iphoneos",
+    "es-macos",
+    "es-tvos",
+    "es_419",
+    "es_419-iphoneos",
+    "es_419-macos",
+    "es_419-tvos",
+    "es_419~applewatch",
+    "es_419~ipad",
+    "es_419~iphone",
+    "es_MX",
+    "es~applewatch",
+    "es~ipad",
+    "es~iphone",
+  ],
+  Albanian: [
+    "sq",
+  ],
+  Arabic: [
+    "ar",
+    "ar-iphoneos",
+    "ar-macos",
+    "ar-tvos",
+    "ar~applewatch",
+    "ar~ipad",
+    "ar~iphone",
+  ],
+  Armenian: [
+    "hy",
+  ],
+  Azerbaijani: [
+    "az",
+  ],
+  Bengali: [
+    "bn",
+  ],
+  Bulgarian: [
+    "bg",
+    "bg-iphoneos",
+    "bg-macos",
+    "bg-tvos",
+    "bg~applewatch",
+    "bg~iphone",
+  ],
+  Burmese: [
+    "my",
+  ],
+  Catalan: [
+    "ca",
+    "ca-iphoneos",
+    "ca-macos",
+    "ca-tvos",
+    "ca~applewatch",
+    "ca~ipad",
+    "ca~iphone",
+  ],
+  "Central Khmer": [
+    "km",
+  ],
+  Croatian: [
+    "hr",
+    "hr-iphoneos",
+    "hr-macos",
+    "hr-tvos",
+    "hr~applewatch",
+    "hr~ipad",
+    "hr~iphone",
+  ],
+  Czech: [
+    "cs",
+    "cs-iphoneos",
+    "cs-macos",
+    "cs-tvos",
+    "cs~applewatch",
+    "cs~ipad",
+    "cs~iphone",
+  ],
+  Danish: [
+    "da",
+    "da-iphoneos",
+    "da-macos",
+    "da-tvos",
+    "da~applewatch",
+    "da~ipad",
+    "da~iphone",
+  ],
+  Dutch: [
+    "Dutch",
+    "nl",
+    "nl-iphoneos",
+    "nl-macos",
+    "nl-tvos",
+    "nl~applewatch",
+    "nl~ipad",
+    "nl~iphone",
+  ],
+  Georgian: [
+    "ka",
+  ],
+  Greek: [
+    "el",
+    "el-iphoneos",
+    "el-macos",
+    "el-tvos",
+    "el~applewatch",
+    "el~ipad",
+    "el~iphone",
+  ],
+  Estonian: [
+    "et",
+  ],
+  Filipino: [
+    "fil",
+  ],
+  Finnish: [
+    "fi",
+    "fi-iphoneos",
+    "fi-macos",
+    "fi-tvos",
+    "fi~applewatch",
+    "fi~ipad",
+    "fi~iphone",
+  ],
+  Gujarati: [
+    "gu_Latn",
+  ],
+  Hebrew: [
+    "he",
+    "he-iphoneos",
+    "he-macos",
+    "he-tvos",
+    "he~applewatch",
+    "he~ipad",
+    "he~iphone",
+  ],
+  Hindi: [
+    "hi",
+    "hi-iphoneos",
+    "hi-macos",
+    "hi-tvos",
+    "hi_Latn",
+    "hi~applewatch",
+    "hi~ipad",
+    "hi~iphone",
+  ],
+  Hungarian: [
+    "hu",
+    "hu-iphoneos",
+    "hu-macos",
+    "hu-tvos",
+    "hu~applewatch",
+    "hu~ipad",
+    "hu~iphone",
+  ],
+  Icelandic: [
+    "is",
+  ],
+  Indonesian: [
+    "id",
+    "id-iphoneos",
+    "id-macos",
+    "id-tvos",
+    "id~applewatch",
+    "id~ipad",
+    "id~iphone",
+  ],
+  Kannada: [
+    "kn_Latn",
+  ],
+  Kazakh: [
+    "kk",
+    "kk-iphoneos",
+    "kk-macos",
+    "kk-tvos",
+    "kk~applewatch",
+    "kk~iphone",
+  ],
+  Korean: [
+    "ko",
+    "ko-iphoneos",
+    "ko-macos",
+    "ko-tvos",
+    "ko~applewatch",
+    "ko~ipad",
+    "ko~iphone",
+  ],
+  Lao: [
+    "lo",
+  ],
+  Latvian: [
+    "lv",
+  ],
+  Lithuanian: [
+    "lt",
+  ],
+  Malayalam: [
+    "ml_Latn",
+  ],
+  Marathi: [
+    "mr",
+  ],
+  Malay: [
+    "ms",
+    "ms-iphoneos",
+    "ms-macos",
+    "ms-tvos",
+    "ms~applewatch",
+    "ms~ipad",
+    "ms~iphone",
+  ],
+  Mongolian: [
+    "mn",
+  ],
+  Norwegian: [
+    "no",
+    "no-iphoneos",
+    "no-macos",
+    "no-tvos",
+    "no~applewatch",
+    "no~ipad",
+    "no~iphone",
+  ],
+  "Norwegian Bokmål": [
+    "nb",
+  ],
+  Oriya: [
+    "or_Latn",
+  ],
+  Punjabi: [
+    "pa",
+  ],
+  Polish: [
+    "pl",
+    "pl-iphoneos",
+    "pl-macos",
+    "pl-tvos",
+    "pl~applewatch",
+    "pl~ipad",
+    "pl~iphone",
+  ],
+  Portuguese: [
+    "pt",
+    "pt-PT",
+    "pt-iphoneos",
+    "pt-macos",
+    "pt_BR",
+    "pt_BR-iphoneos",
+    "pt_BR-macos",
+    "pt_BR-tvos",
+    "pt_BR~ipad",
+    "pt_BR~iphone",
+    "pt_PT",
+    "pt_PT-iphoneos",
+    "pt_PT-macos",
+    "pt_PT-tvos",
+    "pt_PT~applewatch",
+    "pt_PT~ipad",
+    "pt_PT~iphone",
+    "pt~applewatch",
+    "pt~ipad",
+    "pt~iphone",
+  ],
+  Romanian: [
+    "ro",
+    "ro-iphoneos",
+    "ro-macos",
+    "ro-tvos",
+    "ro~applewatch",
+    "ro~ipad",
+    "ro~iphone",
+  ],
+  Russian: [
+    "ru",
+    "ru-iphoneos",
+    "ru-macos",
+    "ru-tvos",
+    "ru~applewatch",
+    "ru~ipad",
+    "ru~iphone",
+  ],
+  Slovak: [
+    "sk",
+    "sk-iphoneos",
+    "sk-macos",
+    "sk-tvos",
+    "sk~applewatch",
+    "sk~ipad",
+    "sk~iphone",
+  ],
+  Swedish: [
+    "sv",
+    "sv-iphoneos",
+    "sv-macos",
+    "sv-tvos",
+    "sv~applewatch",
+    "sv~ipad",
+    "sv~iphone",
+  ],
+  Tamil: [
+    "ta",
+  ],
+  Thai: [
+    "th",
+    "th-iphoneos",
+    "th-macos",
+    "th-tvos",
+    "th~applewatch",
+    "th~ipad",
+    "th~iphone",
+  ],
+  Turkish: [
+    "tr",
+    "tr-iphoneos",
+    "tr-macos",
+    "tr-tvos",
+    "tr~applewatch",
+    "tr~ipad",
+    "tr~iphone",
+  ],
+  Ukrainian: [
+    "uk",
+    "uk-iphoneos",
+    "uk-macos",
+    "uk-tvos",
+    "uk~applewatch",
+    "uk~ipad",
+    "uk~iphone",
+  ],
+  Urdu: [
+    "ur",
+  ],
+  Uzbek: [
+    "uz",
+  ],
+  Vietnamese: [
+    "vi",
+    "vi-iphoneos",
+    "vi-macos",
+    "vi-tvos",
+    "vi~applewatch",
+    "vi~ipad",
+    "vi~iphone",
+  ],
+  "Simplified Chinese": [
+    "zh-Hans",
+    "zh_CN",
+    "zh_CN-iphoneos",
+    "zh_CN-macos",
+    "zh_CN-tvos",
+    "zh_CN~applewatch",
+    "zh_CN~ipad",
+    "zh_CN~iphone",
+  ],
+  "Traditional Chinese": [
+    "zh-Hant",
+    "zh_HK",
+    "zh_HK-iphoneos",
+    "zh_HK-macos",
+    "zh_HK-tvos",
+    "zh_HK~applewatch",
+    "zh_HK~ipad",
+    "zh_HK~iphone",
+    "zh_TW",
+    "zh_TW-iphoneos",
+    "zh_TW-macos",
+    "zh_TW-tvos",
+    "zh_TW~applewatch",
+    "zh_TW~ipad",
+    "zh_TW~iphone",
+  ],
+  Cantonese: [
+    "yue-CN",
+  ],
+};
