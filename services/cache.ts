24a55f564aca89a7ffc5109a6065f88a1ce8bad7
kishikawa katsumi <kishikawakatsumi@mac.com>
2023-09-24 22:45:13 +0900

    Refactor

--- services/cache.ts
+++ services/cache.ts
@@ -1,6 +1,10 @@
 import { connect, Redis } from "../deps.ts";
+import "https://deno.land/std/dotenv/load.ts";
 
 export async function get(key: string) {
+  if (Deno.env.get("DISABLE_CACHE") === "true") {
+    return null;
+  }
   try {
     const redis = await connection();
     const value = await redis.get(key);
@@ -13,13 +17,16 @@ export async function get(key: string) {
 }
 
 export async function set(key: string, value: string) {
+  if (Deno.env.get("DISABLE_CACHE") === "true") {
+    return;
+  }
   try {
     const redis = await connection();
     await redis.set(key, value);
     redis.close();
   } catch (error) {
     console.error(error);
-    return null;
+    return;
   }
 }
 
