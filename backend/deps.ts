7c7377af9b81b8c4eda53720ef1319f259d68330
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2023-09-26 08:14:28 +0900

    Update Eta

--- backend/deps.ts
+++ backend/deps.ts
@@ -11,6 +11,6 @@ export type {
   RouterContext,
   State,
 } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-export { Eta } from "https://deno.land/x/eta@v3.1.0/src/index.ts";
+export { Eta } from "https://deno.land/x/eta@v3.1.1/src/index.ts";
 export { Client, Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 export * from "https://deno.land/std@0.202.0/dotenv/load.ts";
