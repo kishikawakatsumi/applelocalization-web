7304b1291dcdde9bcb8b92991399840869363053
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2023-09-27 11:33:41 +0900

    Refactor

--- frontend/index.js
+++ frontend/index.js
@@ -4,6 +4,7 @@ import "bulma/css/bulma.min.css";
 import "tabulator-tables/dist/css/tabulator_simple.min.css";
 
 import "./css/index.css";
+import "./css/table.css";
 
 import Plausible from "plausible-tracker";
 import "./js/icon.js";
