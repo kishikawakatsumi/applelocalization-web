bc5142901511f3b981aeb6f962ca6ab8d59f20fa
GitHub <noreply@github.com>
2024-02-08 19:43:08 +0000

    Update dependencies

--- backend/deps.ts
+++ backend/deps.ts
@@ -13,4 +13,4 @@ export type {
 } from "https://deno.land/x/oak@v13.1.0/mod.ts";
 export { Eta } from "https://deno.land/x/eta@v3.2.0/src/index.ts";
 export { Client, Pool } from "https://deno.land/x/postgres@ls/mod.ts";
-export * from "https://deno.land/std@0.214.0/dotenv/load.ts";
+export * from "https://deno.land/std@0.215.0/dotenv/load.ts";
