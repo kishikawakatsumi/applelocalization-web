b4b90dd4ee514a5b48085010174459bd1193a09d
GitHub <noreply@github.com>
2025-01-03 19:43:11 +0000

    Update dependencies

--- backend/deps.ts
+++ backend/deps.ts
@@ -5,12 +5,12 @@ export {
   send,
   Status,
   STATUS_TEXT,
-} from "https://deno.land/x/oak@v17.1.3/mod.ts";
+} from "https://deno.land/x/oak@v17.1.4/mod.ts";
 export type {
   RouteParams,
   RouterContext,
   State,
-} from "https://deno.land/x/oak@v17.1.3/mod.ts";
+} from "https://deno.land/x/oak@v17.1.4/mod.ts";
 export { Eta } from "https://deno.land/x/eta@v3.5.0/src/index.ts";
 export { Client, Pool } from "https://deno.land/x/postgres@ls/mod.ts";
 export * from "https://deno.land/std@0.224.0/dotenv/load.ts";
